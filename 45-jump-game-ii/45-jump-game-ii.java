750ad5a4ad11709a1cdcef0d80db1f76d4a27572
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-12 23:55:00 +0600

    Time: 3 ms (53.05%), Space: 46.1 MB (30.11%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        if(nums.length==1){
+            return 0;
+        }
+        
+        int jump = 0;
+        int maxPos = 1;
+        int prePos = 1;
+        
+        for(int i=1;i<=nums.length;i++){
+            
+            
+            maxPos = Math.max(maxPos,i+nums[i-1]);
+            
+            if(prePos == i){
+                prePos = maxPos;
+                jump++;
+            }
+            
+            
+            if(prePos >= nums.length){
+                return jump;
+            }
+        }
+        
+        return jump;
+        
+    }
+}
