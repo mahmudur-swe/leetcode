7a4542ca72c0f0167521d27e1e4ff89bd70fd863
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-13 19:41:17 +0600

    Time: 19 ms (25.09%), Space: 41 MB (12.38%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    
+    
+    public String countAndSay(int n) {
+        
+        String s = "1";
+        
+        for(int i=1;i<n;i++){
+            StringBuilder tmp = new StringBuilder();
+            //System.out.println(s);
+            int cnt = 1;
+            
+            for(int j=0;j<s.length();j++){
+                
+                if(j+1 == s.length()){
+                    
+                    tmp.append(cnt+"");
+                    tmp.append(s.charAt(j));
+                    cnt = 1;
+                    
+                    break;
+                }
+                
+                if(s.charAt(j) == s.charAt(j+1)){
+                    cnt++;
+                }else{
+                    tmp.append(cnt+"");
+                    tmp.append(s.charAt(j));
+                    cnt = 1;
+                }
+                
+            }
+            s = tmp.toString();
+            
+        }
+        
+        
+        return s;
+    }
+
+}
