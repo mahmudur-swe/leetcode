7c306b07e9f653424a20458b926600e294746c02
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-13 22:22:47 +0600

    Time: 2 ms (11.95%), Space: 39.1 MB (30.03%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -19,39 +19,18 @@ class Solution {
         }
         
         double ans = 1.0;
-        double next = 1.0*Math.abs(x);
-        
-        boolean isPos = true;
         boolean isEnv = false;
-        
         if(n<0){
             isEnv = true;
         }
-        
-        if(x<0 && n%2==0){
-            isPos = true;
-        }
-        
-        //System.out.println(-13%2);
-        
-        if(x<0 && (n%2==1 || n%2==-1)){
-            isPos = false;
-        }
-        
-        // if(n==Integer.MIN_VALUE){
-        //     n = n/2;
-        //     next = next*next;
-        // }
-        
-        //n = Math.abs(n);
-        
+            
         while(n!=0){
         
              if(n%2==1 || n%2==-1){
-                ans = ans*next;
+                ans = ans*x;
             }
             
-            next = (next*next);
+            x = (x*x);
             
             n = n/2;
         }
@@ -59,11 +38,7 @@ class Solution {
         if(isEnv){
             ans = 1/ans;
         }
-        
-        if(!isPos){
-            ans = -ans;
-        }
-        
+
         
         return ans;
     }
