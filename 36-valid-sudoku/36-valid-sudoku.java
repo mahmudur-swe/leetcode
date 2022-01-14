e49b33e40431f29c6ac6845fa82e54a94ca271d9
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-15 01:08:57 +0600

    Time: 3 ms (64.84%), Space: 43.1 MB (31.04%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -40,40 +40,9 @@ class Solution {
         
     }
     
-    private int getBoxIndex(int i,int j){
+    private int getBoxIndex(int r,int c){
         
-        if(j<3){
-            
-            if(i<3){
-                return 0;
-            }else if(i<6){
-                return 3;
-            }else if(i<9){
-                return 6;
-            }
-            
-        }else if(j<6){
-            
-            if(i<3){
-                return 1;
-            }else if(i<6){
-                return 4;
-            }else if(i<9){
-                return 7;
-            }
-            
-        }else if(j<9){
-            
-            if(i<3){
-                return 2;
-            }else if(i<6){
-                return 5;
-            }else if(i<9){
-                return 8;
-            }
-            
-        }
+        return (r / 3) * 3 + c / 3;
         
-        return 0;
     }
 }
