97055db92d8b5da567fc7077c49ddbd1a6bd1559
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-17 13:45:15 +0600

    Time: 30 ms (5.47%), Space: 52.9 MB (8.50%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        
+        if(intervals.length<=1){
+            return intervals;
+        }
+        
+        Arrays.sort(intervals,Comparator.comparingInt(a -> a[0]));
+  
+        int[] last = intervals[0];
+        
+        List<int[]> ans = new ArrayList();
+        
+        for(int i=1;i<intervals.length;i++){
+            
+            if(intervals[i][0]>last[1]){
+                ans.add(last);
+                last = intervals[i];
+            }else{
+                last[1] = Math.max(intervals[i][1],last[1]);
+            }
+        }
+        
+        ans.add(last);
+        
+        int[][] res = new int[ans.size()][2];
+        ans.toArray(res);
+        
+        
+        return res;
+    }
+}
