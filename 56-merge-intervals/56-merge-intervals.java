187ead4768791172181ff4b3641744bd54051e26
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-17 13:48:35 +0600

    Time: 18 ms (16.02%), Space: 52.7 MB (17.04%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,32 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
+        if(intervals.length<=1){
+            return intervals;
+        }
+        
+        Arrays.sort(intervals,Comparator.comparingInt(a -> a[0]));
+  
+        int[] last = intervals[0];
         
         List<int[]> ans = new ArrayList();
         
-        int[] current = null;
-        
-        current = intervals[0];
-        
         for(int i=1;i<intervals.length;i++){
             
-            int[] merge = merge(current,intervals[i]);
-            
-            if(merge == null){
-                //System.out.println("c1 "+current[0]+" " + current[1]);
-                ans.add(current);
-                current = intervals[i];
-                 //System.out.println("c2 "+current[0]+" " + current[1]);
+            if(intervals[i][0]>last[1]){
+                ans.add(last);
+                last = intervals[i];
             }else{
-                //System.out.println(merge[0]+" "+merge[1]);
-                current = merge;
+                last[1] = Math.max(intervals[i][1],last[1]);
             }
         }
         
-        if(current!=null){
-            ans.add(current);
-        }
+        ans.add(last);
         
         int[][] result = new int[ans.size()][2];
         
@@ -36,16 +31,4 @@ class Solution {
         
         return result;
     }
-    
-    public int[] merge(int[] l1,int[] l2){
-        
-        if(l2[0]>=l1[0] && l2[0]<=l1[1]){
-            l1[0] = Math.min(l1[0],l2[0]);
-            l1[1] = Math.max(l1[1],l2[1]);
-            return l1;
-        }
-        
-        return null;
-    }
-    
 }
