57793102a073e3f43e3f1ea7732f33781f59871c
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-17 22:13:11 +0600

    Time: 5 ms (34.12%), Space: 43.7 MB (22.58%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,75 +1,32 @@
 class Solution {
-    
-    boolean[] isVisited;
-    
-    private boolean getAns(int p,int v,List<Integer>[] list){
-        
-        isVisited[v] = true;
-        
-        //System.out.println(p+" "+v);
-        
-        for(Integer x:list[v]){
-            if(!isVisited[x]){
-                if(getAns(v,x,list)){
-                    return true;
-                }
-            }else if(x!=p){
-                return true;
-            }
-        }
-        return false;
-    }
-    
     public boolean validTree(int n, int[][] edges) {
-   
-        if(n==1){
-            return true;
-        }
-        
-        isVisited = new boolean[n];
-        
-        //Arrays.sort(edges, Comparator.comparingInt(a -> a[0]));
-        
-        List<Integer>[] list = new List[n];
-        
-        for(int[] edge:edges){
-            
-            if(list[edge[0]] ==null){
-                list[edge[0]] = new ArrayList();
-            }
-            
-            if(list[edge[1]] ==null){
-                list[edge[1]] = new ArrayList();
-            }
-            
-            list[edge[0]].add(edge[1]);
-            
-            list[edge[1]].add(edge[0]);
-            
-        }
-        
-        boolean ans = true;
-        
-        int startCount = 0;
-        
-        if(!isVisited[0] && list[0]!=null && list[0].size()>0){   
-            if(getAns(-1,0,list)){
-                return false;
-            }  
-        }
-        
-        
-        for(int i=0;i<n;i++){
-            
-            if(!isVisited[i]){
-                return false;
-            }
-            
-        }
-        
-        
-        return true;
-        
-        
+    
+    if (edges.length != n - 1) return false;
+    
+    // Make the adjacency list.
+    List<List<Integer>> adjacencyList = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+        adjacencyList.add(new ArrayList<>());
     }
+    for (int[] edge : edges) {
+        adjacencyList.get(edge[0]).add(edge[1]);
+        adjacencyList.get(edge[1]).add(edge[0]);
+    }
+    
+    Queue<Integer> queue = new LinkedList<>();
+    Set<Integer> seen = new HashSet<>();
+    queue.offer(0);
+    seen.add(0);
+    
+    while (!queue.isEmpty()) {
+        int node = queue.poll();
+        for (int neighbour : adjacencyList.get(node)) {
+            if (seen.contains(neighbour)) continue;
+            seen.add(neighbour);
+            queue.offer(neighbour);
+        }
+    }
+    
+    return seen.size() == n;   
+}
 }
