7e5f0b2f6f44c615a838ef7487a5c86f6c97ab48
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-20 02:53:06 +0600

    Time: 270 ms (5.06%), Space: 142.9 MB (5.25%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,68 +1,125 @@
 class Solution {
-    // maximum number of jobs are 50000
-    int[] memo = new int[50001];
     
-    private int findNextJob(int[] startTime, int lastEndingTime) {
-        int start = 0, end = startTime.length - 1, nextIndex = startTime.length;
-        
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (startTime[mid] >= lastEndingTime) {
-                nextIndex = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+    class Node{
+        int start;
+        int end;
+        int profit;
+        public Node(int start,int end,int profit){
+            
+            this.start = start;
+            this.end = end;
+            this.profit = profit;
+            
         }
-        return nextIndex;
     }
     
-    private int findMaxProfit(List<List<Integer>> jobs, int[] startTime, int n, int position) {
-        // 0 profit if we have already iterated over all the jobs
-        if (position == n) {
+    Map<Integer,Integer> dp;
+    
+    
+    Map<Integer,List<Node>> map;
+    
+    List<Node> nodes;
+    
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        
+        dp = new HashMap();
+        
+        nodes = new ArrayList();
+        
+        map = new HashMap();
+        
+        for(int i=0;i<startTime.length;i++){
+            nodes.add(new Node(startTime[i],endTime[i],profit[i]));
+        }
+        
+        Collections.sort(nodes,new Comparator<Node>(){
+            public int compare(Node node1,Node node2){
+                
+//                 if(node1.start == node2.start){
+                    
+//                     if(node1.end == node2.end){
+                        
+//                         return node2.profit - node1.profit;
+//                     }
+                    
+//                     return node1.end - node2.end;
+                    
+//                 }
+                
+                return node1.start-node2.start;
+                
+            }
+        });
+        
+        int n = nodes.get(nodes.size()-1).start;
+        int start = nodes.get(0).start;
+        
+        
+        for(Node node:nodes){
+            List<Node> ns = map.getOrDefault(node.start,new ArrayList());
+            ns.add(node);
+            map.put(node.start,ns);
+            
+        }
+        
+        return getAns(start,n);
+    }
+    
+    public int getAns(int pos,int n){
+        
+        if(pos>n){
             return 0;
         }
         
-        // return result directly if it's calculated 
-        if (memo[position] != -1) {
-            return memo[position];
+        if(dp.get(pos)!=null){
+            return dp.get(pos);
         }
         
-        // nextIndex is the index of next non-conflicting job
-        int nextIndex = findNextJob(startTime, jobs.get(position).get(1));
+        int x = 0;
         
-        // find the maximum profit of our two options: skipping or scheduling the current job
-        int maxProfit = Math.max(findMaxProfit(jobs, startTime, n, position + 1), 
-                        jobs.get(position).get(2) + findMaxProfit(jobs, startTime, n, nextIndex));
+        if(map.get(pos) != null){
+            
+            for(Node node:map.get(pos)){
+                
+                x = Math.max(x, node.profit + getAns(node.end,n));
+                
+            }
+            
+        }
         
-        // return maximum profit and also store it for future reference (memoization)
-        return memo[position] = maxProfit;
+        Node nxt = getNextNode(pos);
+        
+        if(pos!=nxt.start)
+        {
+            x = Math.max(x,getAns(nxt.start,n));
+        }
+        
+        dp.put(pos,x);
+        
+        return x;
+        
     }
     
-    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        List<List<Integer>> jobs = new ArrayList<>();
+    private Node getNextNode(int endTime){
         
-        // marking all values to -1 so that we can differentiate 
-        // if we have already calculated the answer or not
-        Arrays.fill(memo, -1);
+        int start = 0;
+        int end = nodes.size()-1;
         
-        // storing job's details into one list 
-        // this will help in sorting the jobs while maintaining the other parameters
-        int length = profit.length;
-        for (int i = 0; i < length; i++) {
-            ArrayList<Integer> currJob = new ArrayList<>();
-            currJob.add(startTime[i]);
-            currJob.add(endTime[i]);
-            currJob.add(profit[i]);
-            jobs.add(currJob);
-        }
-        jobs.sort(Comparator.comparingInt(a -> a.get(0)));
-        
-        // binary search will be used in startTime so store it as separate list
-        for (int i = 0; i < length; i++) {
-            startTime[i] = jobs.get(i).get(0);
+        while(start<end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(nodes.get(mid).start <= endTime){
+                start = mid+1;
+            }else if(nodes.get(mid).start>endTime){
+                end = mid;
+            }
+            
         }
         
-        return findMaxProfit(jobs, startTime, length, 0);
+        return nodes.get(end);
+        
     }
+    
+    
 }
