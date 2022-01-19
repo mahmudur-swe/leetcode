fb8dfd3e5aa6020f64350bb9f4b95ef9cc39d273
Md Mahmudur Rahman <tanim_57@live.com>
2022-01-20 01:10:45 +0600

    Time: 0 ms (100.00%), Space: 41.7 MB (39.12%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -20,8 +20,6 @@ public class Solution {
             return head;
         }
         
-        int total = 1;
-        
         ListNode slow = head.next;
         ListNode fast = head.next.next;
         
@@ -30,63 +28,22 @@ public class Solution {
             if(slow.next == null || fast.next == null || fast.next.next == null){
                 return null;
             }
-            
-            total++;
+         
             slow = slow.next;
             fast = fast.next.next;
             
         }
         
-        //System.out.println("slow "+ slow.val);
         
-      
-        
-        if(slow.next == slow){
-            return slow;
-        }
-        
-        if(slow.next.next == slow){
-            return slow;
-        }
-        
-        int count = 0;
-        
-        fast = slow;
-        
-        Set<ListNode> nodes = new HashSet();
-        
-        while(true){
+        while(head!=fast){
             
-            if(count>0 && slow == fast){
-                break;
-            }
-            nodes.add(fast);
-            count++;
+            head = head.next;
             fast = fast.next;
                         
         }
         
-        while(true){
-            
-            if(nodes.contains(head)){
-                return head;
-            }
-            
-            head = head.next;
-            
-        }
+        return head;
         
-        //System.out.println(total+" "+count);
-        
-        //return slow;
-        
-//         ListNode root = head;
-        
-//         for(int i=0;i<k;i++){
-            
-//             root = root.next;
-            
-//         }
         
     }
 }
