91f88517070799f98621217747bfa45758e490c1
Md Mahmudur Rahman <tanim_57@live.com>
2022-02-09 01:02:07 +0600

    Time: 14 ms (49.84%), Space: 44.6 MB (13.71%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        if(x<10){
+            return true;
+        }
+        
+        long input = x;
+        long reverse = 0;
+        
+        while(input>0){
+            
+            reverse = reverse*10 + (input%10);
+            input /= 10;
+            
+        }
+        
+        //System.out.println(reverse);
+        
+        return ((long) x ) == reverse;
+    }
+}
+
+/*
+
+Time = O(1)
+Space = O(1)
+
+If negtive return false
+
+if positive -> reverse the integer and assign to a long variable
+and check weather two are same or not.
+
+same = true
+
+*/
