26e45b50317b64b3a3f9a8dd4f79a015de80d6c1
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-02 10:22:29 +0600

    Time: 2 ms (27.93%), Space: 57.2 MB (70.79%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,52 +1,48 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
+        int length = nums.length;
 
-    int length = nums.length;
+        int numsLength = nums.length;
 
-    int start = 0;
+        int start = 0;
 
-     while(true){
+        while (true) {
 
-        if(length <= 1){
-            return;
-        }
-        
-        k = k % length;
-
-        if(k==0){
-            return;
-        }
-  
-
-        int tmp;
-
-        for(int i=start;i<(nums.length - k) ; i=i+k){
-
-            int j = nums.length - k;
-            
-            if(i+k>nums.length){
-                break;
+            if (length <= 1) {
+                return;
             }
-            
-            for(int v=0; v < k;v++){
-                tmp = nums[i+v];
-                nums[i+v] = nums[j];
-                nums[j] = tmp;
-                j++;
+
+            k = k % length;
+
+            if (k == 0) {
+                return;
             }
+
+            int tmp;
+
+            for (int i = start; i < (numsLength - k); i = i + k) {
+
+                int j = numsLength - k;
+
+                if (i + k > numsLength) {
+                    break;
+                }
+
+                for (int v = 0; v < k; v++) {
+                    tmp = nums[i + v];
+                    nums[i + v] = nums[j];
+                    nums[j] = tmp;
+                    j++;
+                }
+            }
+
+            int newlength = length % k;
+
+            start = start + (length - newlength);
+
+            length = newlength;
+
         }
-
-       // if(nums.length % 2==0 || k % 2 == 0){
-         //   return;
-        //}
-   
-        int newlength = length % k;
-
-        start = start + (length - newlength);
-        length = newlength;
-        
-
-    }
     }
 }
