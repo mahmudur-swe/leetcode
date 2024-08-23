d26741c88ba2aefb81c77c7ba873739d640ef496
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-24 00:50:50 +0600

    Time: 10 ms (6.7%), Space: 45.6 MB (6.22%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -24,29 +24,18 @@ class Solution {
             }
         }
 
-    
-    
-
         List<Integer> res2 = new ArrayList();
 
         res2.addAll(values);
 
         combinationSum(candidates, target, pos+1, res2);
 
-        boolean ifAdded = false;
-
         List<Integer> res3 = new ArrayList();
 
         res3.addAll(values);
-        res3.add(candidates[pos]);
+        res2.add(candidates[pos]);
 
-        combinationSum(candidates, target - candidates[pos], pos, res3);
-
-        List<Integer> res4 = new ArrayList();
-
-       
-
-   
+        combinationSum(candidates, target - candidates[pos], pos, res2);
 
 
     }
