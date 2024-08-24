3231261579513d4d78807750b34f429c0ff09bf1
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-24 16:58:52 +0600

    Time: 5 ms (10.92%), Space: 47.5 MB (6.23%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,37 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-
         int m = grid.length;
         int n = grid[0].length;
-
-        int[][] visited = new int[m+1][n+1];
-
+        int[][] sum = new int[m+1][n+1];
         for(int i=0;i<=m;i++){
-             visited[i][0] = 200*n*m+1;
+            sum[i][0] = Integer.MAX_VALUE;
         }
-
-        for(int j=0;j<=n;j++){
-            visited[0][j] = 200*n*m+1;
+        for(int i=0;i<=n;i++){
+            sum[0][i] = Integer.MAX_VALUE;
         }
-
-
-        visited[1][0] = 0;
-        visited[0][1] = 0;
-
-        visited[1][1] = 200*n*m+1;
-
+        
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                visited[i][j] = Math.min(grid[i-1][j-1]+visited[i-1][j],grid[i-1][j-1]+visited[i][j-1]);
-                //visited[i][j] = Math.min(grid[i-1][j-1]+visited[i][j-1],visited[i][j]);
-
+                if(i==1 && j==1){
+                    sum[i][j] = grid[i-1][j-1];
+                }else{
+                    sum[i][j] = Math.min(sum[i-1][j],sum[i][j-1])+grid[i-1][j-1];
+                }
+           
             }
         }
-
-        return visited[m][n];
-
-
-
+        
+        return sum[m][n];
+        
         
     }
 }
