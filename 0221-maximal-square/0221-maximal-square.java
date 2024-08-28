04592ec41704c145461d43a02cd1d98e5a6e9aff
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-28 21:27:09 +0600

    Time: 50 ms (9.1%), Space: 57.4 MB (55.85%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,70 +1,55 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-
+        
         int m = matrix.length;
         int n = matrix[0].length;
-
-        int[][] sum = new int [m+1][n+1];
-
-        // for(int i=1;i<m;i++){
-        //     matrix[i][0] += matrix[i-1][0]; 
-        // }
-
-        // for(int i=1;i<n;i++){
-        //     matrix[0][i] += matrix[0][i-1]; 
-        // }
-
-        int ans = 0;
-
+        
+        int[][] sum = new int[m+1][n+1];
+        
         for(int i=1;i<=m;i++){
-
-            for(int j = 1; j<=n; j++){
+            for(int j=1;j<=n;j++){
+                int val = matrix[i-1][j-1] == '1'? 1:0;
                 
-                if(matrix[i-1][j-1] == '0'){
-                    sum[i][j] = 0;
-                }else{
-                    sum[i][j] = 1;
-                }
+                sum[i][j] = val + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
                 
-                sum[i][j] += (sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] );
-
-               // System.out.printf("%d %d: %d\n",i,j,sum[i][j]);
+                //System.out.println(i+ " " + j + " "+ sum[i][j]);
                 
             }
         }
-
-        for(int i=0;i<=m;i++){
-            for(int j = 0; j<=n;j++){
-
-                 int k = i+1;
-                 int l = j+1;
-
-                 
-                 while(k<=m && l<=n){
-
-                    int squareValue = (sum[k][l] - sum[i][l] - sum[k][j] + sum[i][j]);
-
-                    int sumValue = (k-i)*(k-i);
-
-                    //System.out.printf("%d %d: %d %d %d, %d\n",i,j,k,l,squareValue,sumValue);
-
-                    if(squareValue == sumValue){
-                        ans = Math.max(ans,squareValue);
+        
+        int ans = 0;
+        
+        for(int i=1;i<=m;i++){
+            
+            for(int j=1;j<=n;j++){
+               
+                int x = i;
+                int y = j;
+                
+                while(x<=m && y<=n){
+                    
+                    int square = sum[x][y] - sum[i-1][y] - sum[x][j-1] + sum[i-1][j-1];
+                    
+                    int l = x-i+1;
+                    int h = y-j+1;
+                    
+                    // System.out.println(i + " " + j + " " + x + " "+ y + " " + square);
+                    // System.out.println(sum[x][y]+" " + sum[i][y] + " " + sum[x][j] + " "+sum[i-1][j-1]);
+                    
+                    if(l*h == square){
+                        ans = Math.max(ans,square);
                     }else{
                         break;
                     }
                     
-                    k++;
-                    l++;
-
-                 }
-
+                    x++;
+                    y++;
+                    
+                }
+                
+                
             }
         }
-
-
-
         return ans;
-        
     }
 }
