68edc815a2c7f2b5fd800427b9c2eea7d6697519
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-29 12:10:31 +0600

    Time: 1 ms (23.1%), Space: 41.7 MB (49.86%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -7,29 +7,28 @@ class Solution {
         int[][] dp = new int[m+1][n+1];
 
 
-
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                dp[i][j] = -1;
-                // if(i==1 && j==1){
-                //     if(obstacleGrid[0][0]==1){
-                //         dp[1][1] = 0;
-                //     }else{
-                //         dp[1][1] = 1;
-                //     }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+          
+                if(i==1 && j==1){
+                    if(obstacleGrid[0][0]==1){
+                        dp[1][1] = 0;
+                    }else{
+                        dp[1][1] = 1;
+                    }
                     
-                // }else{
-                //     if(obstacleGrid[i-1][j-1] == 1){
-                //         dp[i][j] = 0;
-                //     }else{
-                //         dp[i][j] += dp[i-1][j];
-                //         dp[i][j] += dp[i][j-1];
-                //     }
-                // }
+                }else{
+                    if(obstacleGrid[i-1][j-1] == 1){
+                        dp[i][j] = 0;
+                    }else{
+                        dp[i][j] += dp[i-1][j];
+                        dp[i][j] += dp[i][j-1];
+                    }
+                }
             }
         }
 
-        return totalPath(obstacleGrid,dp,0,0);
+        return dp[m][n];
 
 
 
