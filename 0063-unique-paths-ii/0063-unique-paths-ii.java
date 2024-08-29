2fd6cf922b665d25db8889b015902df75515e78e
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-29 12:06:03 +0600

    Time: 1 ms (23.1%), Space: 42 MB (19.04%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int m= obstacleGrid.length;
+        int n= obstacleGrid[0].length;
+
+        int[][] dp = new int[m+1][n+1];
+
+
+        dp[1][1] = 0;
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 && j==1){
+                    if(obstacleGrid[0][0]==1){
+                        dp[1][1] = 0;
+                    }else{
+                        dp[1][1] = 1;
+                    }
+                    
+                }else{
+                    if(obstacleGrid[i-1][j-1] == 1){
+                        
+                        dp[i][j] = 0;
+
+                    }else{
+                        dp[i][j] += dp[i-1][j];
+                        dp[i][j] += dp[i][j-1];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+
+
+
+    }
+
+    public int totalPath(int[][] obstacleGrid,int i,int j){
+
+        if(i==obstacleGrid.length){
+            return 0;
+        }
+
+        if(j== obstacleGrid[0].length){
+            return 0;
+        }
+
+        if(i==obstacleGrid.length-1 && j== obstacleGrid[0].length-1){
+            return 1;
+        }
+
+        if(obstacleGrid[i][j] == 1){
+            return 0;
+        }
+
+        int leftSum = totalPath(obstacleGrid,i+1,j);
+        int rightSum = totalPath(obstacleGrid,i+1,j);
+
+
+
+
+        return leftSum+rightSum;
+
+    }
+
+
+}
