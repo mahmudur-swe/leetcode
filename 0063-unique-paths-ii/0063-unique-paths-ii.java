7084864988bbcae16163ec506b5179fb18a4adc5
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-29 12:09:25 +0600

    Time: 0 ms (100%), Space: 41.5 MB (65.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -7,37 +7,35 @@ class Solution {
         int[][] dp = new int[m+1][n+1];
 
 
-        dp[1][1] = 0;
 
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(i==1 && j==1){
-                    if(obstacleGrid[0][0]==1){
-                        dp[1][1] = 0;
-                    }else{
-                        dp[1][1] = 1;
-                    }
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j] = -1;
+                // if(i==1 && j==1){
+                //     if(obstacleGrid[0][0]==1){
+                //         dp[1][1] = 0;
+                //     }else{
+                //         dp[1][1] = 1;
+                //     }
                     
-                }else{
-                    if(obstacleGrid[i-1][j-1] == 1){
-                        
-                        dp[i][j] = 0;
-
-                    }else{
-                        dp[i][j] += dp[i-1][j];
-                        dp[i][j] += dp[i][j-1];
-                    }
-                }
+                // }else{
+                //     if(obstacleGrid[i-1][j-1] == 1){
+                //         dp[i][j] = 0;
+                //     }else{
+                //         dp[i][j] += dp[i-1][j];
+                //         dp[i][j] += dp[i][j-1];
+                //     }
+                // }
             }
         }
 
-        return dp[m][n];
+        return totalPath(obstacleGrid,dp,0,0);
 
 
 
     }
 
-    public int totalPath(int[][] obstacleGrid,int i,int j){
+    public int totalPath(int[][] obstacleGrid,int[][] dp,int i,int j){
 
         if(i==obstacleGrid.length){
             return 0;
@@ -47,21 +45,25 @@ class Solution {
             return 0;
         }
 
-        if(i==obstacleGrid.length-1 && j== obstacleGrid[0].length-1){
-            return 1;
-        }
-
-        if(obstacleGrid[i][j] == 1){
+         if(obstacleGrid[i][j] == 1){
             return 0;
         }
 
-        int leftSum = totalPath(obstacleGrid,i+1,j);
-        int rightSum = totalPath(obstacleGrid,i+1,j);
+        if(i==obstacleGrid.length-1 && j == obstacleGrid[0].length-1){
+            return 1;
+        }
+
+       
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int leftSum = totalPath(obstacleGrid,dp,i+1,j);
+        int rightSum = totalPath(obstacleGrid,dp,i,j+1);
 
 
-
-
-        return leftSum+rightSum;
+        return dp[i][j] = leftSum+rightSum;
 
     }
 
