9d3e17e87168e353b0a46b4edf153026849144e3
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-08-30 16:50:50 +0600

    Time: 133 ms (8.69%), Space: 110.4 MB (98.41%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,160 @@
+class LRUCache {
+
+    Node[] nodeMapper = new Node[10001];
+
+    int capacity = 0;
+    int currentCacheSize = 0;
+
+    Node startNode = null;
+    Node endNode = null;
+
+    
+    List<String> cacheList = new ArrayList();
+
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+       
+    }
+    
+    public int get(int key) {
+        Node node = nodeMapper[key];
+        if(node!=null){
+            addOrUpdateCache(key,node);
+            return node.data;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+
+        Node node = nodeMapper[key];
+
+        if(node == null){
+            node = new Node(key,value);
+        }else{
+            node.data = value;
+        }
+
+        addOrUpdateCache(key,node);
+        
+    }
+
+
+    private Node removeCache(int key){
+
+        Node maper = nodeMapper[key];
+
+        return removeCache(maper);
+
+    }
+
+    private Node removeCache(Node maper){
+
+        if(maper==null){
+            return null;
+        }
+
+        currentCacheSize--;
+
+        Node start = maper.pre;
+        Node end = maper.next;
+
+        if(start == null){
+            startNode = end;
+            if(startNode!=null){
+                startNode.pre = null;
+            }
+        }else{
+            start.next = end;
+            if(end!=null){
+                end.pre = start;
+            }
+        }
+
+        if(end == null){
+            endNode = start;
+            if(endNode!=null){
+                endNode.next = null;
+            }
+        }else{
+            end.pre = start;
+            if(start!=null){
+                start.next = end;
+            }
+        }
+        
+        nodeMapper[maper.key] = null;
+
+        return maper;
+    }
+
+
+    private void addOrUpdateCache(int key, Node node){
+
+        if(nodeMapper[key] == null && currentCacheSize+1>capacity){
+            
+            System.out.println("Hello World!");
+
+            if(endNode!=null){
+                System.out.println(endNode.data);
+            }
+            
+            removeCache(endNode);
+        }
+
+        if(nodeMapper[key]!=null){
+            removeCache(nodeMapper[key]);
+        }
+
+        currentCacheSize++;
+
+        nodeMapper[key] = node;
+
+        node.pre = null;
+        node.next = startNode;
+
+        if(startNode!=null){
+            startNode.pre = node;
+        }   
+
+        startNode = node;
+        startNode.pre = null;
+
+        if(endNode == null){
+            endNode = startNode;
+            if(endNode!=null){
+                endNode.next = null;
+            }
+        }     
+
+        
+    
+    }
+
+
+
+    static class Node {
+        int data = -1;
+        int key = 0;
+        Node next;
+        Node pre;
+
+        // Constructor
+        public Node(int key,int data) {
+            this.data = data;
+            this.key = key;
+            this.next = null;
+            this.pre = null;
+        }
+    }
+
+
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
