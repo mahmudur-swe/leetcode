f75a1834afd0df9cb423f84c7a1b4dbf2cf46725
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-09-01 00:54:54 +0600

    Time: 13 ms (58.28%), Space: 56.4 MB (72.32%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -0,0 +1,63 @@
+class Solution {
+
+
+    double[] visited;
+
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        visited = new double[n];
+
+
+
+        visited[start_node] = 1;
+
+        int top = start_node;
+
+        for (top = 0; top < n; top++) {
+
+            //if (paths[top] != null) 
+            {
+                
+                boolean hasUpdate = false;
+
+                for (int j = 0; j < edges.length; j++) {
+
+                    
+                    int start = edges[j][0];
+                    int end = edges[j][1];
+                    double prob = succProb[j];
+
+                    if(visited[end] < visited[start] * prob ){
+                        visited[end] = Math.max(visited[end], visited[start] * prob);
+                        hasUpdate = true;
+                    }
+
+                   if(visited[start] < visited[end] * prob ){
+                        visited[start] = Math.max(visited[start], visited[end] * prob);
+                        hasUpdate = true;
+                    }
+
+                }
+
+                if(!hasUpdate){
+                    break;
+                }
+
+                // for (int i = 0; i < paths[top].size(); i++) {
+
+                //     int nextNode = paths[top].get(i);
+
+                //     visited[nextNode] = Math.max(visited[nextNode], visited[top] * probability[top].get(i));
+
+                //     // System.out.printf(top + " " + nextNode +" " + visited[nextNode] + "\n");
+
+                // }
+
+            }
+
+        }
+
+        return visited[end_node];
+
+    }
+
+}
