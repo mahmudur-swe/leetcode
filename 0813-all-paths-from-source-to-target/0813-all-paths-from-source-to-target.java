e795a866aa692ce2bb65ab67abfa6b317c5c7ea7
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-09-02 22:42:22 +0600

    Time: 3 ms (29.33%), Space: 46.5 MB (22.46%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -0,0 +1,81 @@
+class Solution {
+
+    List<List<Integer>>[] path;
+
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        
+        
+        path = new ArrayList[graph.length];
+
+        getPath(graph,0);
+
+        return path[0];
+    }
+
+    private List<List<Integer>> getPath(int[][] graph,int pos){
+
+        if(pos == graph.length-1){
+
+            if(path[pos]==null){
+
+                path[pos] = new ArrayList();    
+
+                List<Integer> a = new ArrayList();
+
+                a.add(pos);
+
+                List<List<Integer>> vv = new ArrayList();
+
+                vv.add(a);
+
+                path[pos] = vv;
+            }
+
+            
+            return path[pos];
+
+        }
+
+        if( path[pos]!=null){
+            return path[pos];
+        }
+
+        if(path[pos]==null){
+            path[pos] = new ArrayList();
+        }
+
+        List<List<Integer>> ans = new ArrayList();
+
+        
+
+
+        for(int i=0;i<graph[pos].length;i++){
+
+            List<List<Integer>> pathValues = getPath(graph,graph[pos][i]);
+
+            for(int j=0;j<pathValues.size();j++){
+                List<Integer> val = new ArrayList();
+
+                val.add(pos);
+
+                List<Integer> pp = pathValues.get(j);
+
+                for(int k=0;k<pp.size();k++){
+                    val.add(pp.get(k));
+                }
+
+                //new ArrayList(p.get(j))
+                ans.add(val);
+            }
+            
+
+        }
+
+        if(ans!=null && ans.size()>0){  
+            path[pos].addAll(ans);
+        }
+
+        return path[pos];
+
+    }
+}
