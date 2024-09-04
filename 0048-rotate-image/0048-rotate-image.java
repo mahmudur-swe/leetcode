0a7ed99ad56772928d0925b2c9186b3f0a3e79e9
Md Mahmudur Rahman <tanim.mr57@gmail.com>
2024-09-05 01:06:48 +0600

    Time: 0 ms (100%), Space: 42.2 MB (45.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -8,20 +8,12 @@ class Solution {
 
             for (int j = offset; j + offset < n; j++) {
 
-                int l = i;
-                int m = j;
+           
 
-                System.out.print("n = " + n + "  " + offset + " : " + "\n");
-                System.out.print(i + "  " + j + " : " + (matrix[i][j]) + "\n");
-                System.out.print(m + "  " + (n - offset) + " : " + (matrix[m][n - offset]) + "\n");
-                System.out.print((n - offset) + "  " + (n - j) + " : "
-                            + (matrix[n - offset][n - j]) + "\n");
-                    System.out.print(
-                            (n - j) + "  " + offset + " : " + (matrix[n - j][offset]) + "\n");
 
-                int tmp = matrix[m][n - offset];
+                int tmp = matrix[j][n - offset];
 
-                matrix[m][n - offset] = matrix[i][j];
+                matrix[j][n - offset] = matrix[i][j];
 
                 int subtmp = matrix[n - offset][n - j];
 
