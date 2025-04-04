736664243b97bf8dcdb0b1b49728e7989336affa
Md Mahmudur Rahman <mahmudur.swe@gmail.com>
2025-04-04 23:22:36 +0600

    Time: 247 ms (23.82%), Space: 66.2 MB (5.18%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,80 +1,70 @@
 class Solution {
 
-    // i = 0 to n-2
-    // j = i+1 to n - 1
-    // k = j + 1 to n-2
-    // i + j + k = 0
-    // -4 -1 -1 0 1 2
-    //  
+    boolean[][] dp;
 
     public List<List<Integer>> threeSum(int[] nums) {
+
+        int length = nums.length;
+        dp = new boolean[length][length];
         
         Arrays.sort(nums);
 
-        int length = nums.length;
-
-        int firstPos = 0;
-
-        for(int i=0;i<length;i++){
-            if(nums[i]>=0){
-                firstPos = i;
-                break;
-            }
-        }
-
         List<List<Integer>> ans = new ArrayList();
 
-        for(int i=0;i<length-2;i++){
-
-            if(nums[i]>0){
-                break;
-            }
-
-            if(i>0){
-                if(nums[i] == nums[i-1]){
-                    continue;
-                }
-            }
-
-            int lastSum = 1;
-
-            for(int j=i+1;j<length-1;j++){
-
-                int sum = nums[i] + nums[j];
-
-                if(nums[j] == nums[j-1] && sum == lastSum ){
-                    continue;
-                }
-
-                lastSum = sum;
-
-                if(sum <=0){
-
-                    int index = Arrays.binarySearch(nums, Math.max(firstPos,j+1), length, -1*sum);
-
-                    if(index>=0){
-                        
-                        List<Integer> result = new ArrayList();
-                        result.add(nums[i]);
-                        result.add(nums[j]);
-                        result.add(nums[index]);
-
-                        ans.add(result);
-                        
-                    }
-                }
-
-                
-
-
-            }
-
-
-
-        }
+        getAns(nums,0,length-1,ans);
 
         return ans;
 
+    }
+
+    void getAns(int[] nums, int start, int end, List<List<Integer>> ans){
+
+        if(end-start<=1){
+            return;
+        }
+
+        if(dp[start][end]){
+            return;
+        }
+        
+        int sum = nums[start] + nums[end];
+
+        int index = Arrays.binarySearch(nums, start+1, end, sum*-1);
+
+        if(index>=0){
+            List<Integer> result = new ArrayList();
+            result.add(nums[start]);
+            result.add(nums[end]);
+            result.add(nums[index]);
+            ans.add(result);
+        }
+
+        dp[start][end] = true;
+
+        int st = start+1;
+
+        while(nums[st] == nums[st-1] && st<end){
+            st++;
+        }
+
+        getAns(nums, st,end,ans);
+
+        int en = end - 1;
+
+        while(nums[en] == nums[en+1] && start<en){
+            en--;
+        }
+
+    
+        getAns(nums, start,en,ans);
+
+      
+
 
     }
+
+
+
+
+
 }
