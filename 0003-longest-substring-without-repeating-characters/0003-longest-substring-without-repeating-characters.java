966d04c652bb5b92f9046e62b4bc47080a3f58f7
Md Mahmudur Rahman <mahmudur.swe@gmail.com>
2025-04-06 21:30:52 +0600

    Time: 3 ms (90.9%), Space: 44.6 MB (67.75%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,32 +1,57 @@
 class Solution {
+
+    // abcabcbb = i = 0 ;
+    // flag to check char already apper or not
+    // goto thofh until repeated char found
+    // then break start from next pos.
+    // Time b c d = n^2
+    // Space O(n)
+
+    // start = 0;
+    // end = 0;
+
+    // flag[a] = 0;
+    // flag [b] = 1, flag c = 2//
+    // flag[a] > start
+    // srart = flag[a] + 1
+    // end = current pos;
+
+
     public int lengthOfLongestSubstring(String s) {
 
-        if(s.length()==0){
+        if(s == null || s.length() == 0){
             return 0;
         }
 
-        int ans  = 0;
 
         int start = 0;
         int end = 0;
-        int[] charPos = new int[200];
-        for(int i=0;i<charPos.length;i++){
-            charPos[i] = -1;
-        }
+
+        int ans = 0;
+
+        int[] flag = new int[500];
+
+        Arrays.fill(flag,-1);
 
         while(end<s.length()){
-            
-            if(charPos[s.charAt(end)]!=-1){
-                start = Math.max(charPos[s.charAt(end)]+1,start);
+
+            char c = s.charAt(end);
+
+            if(flag[c] != -1 && flag[c]>=start){
+                start = flag[c]+1;
             }
 
-            ans = Math.max(ans,end-start+1);
+            
+            flag[c] = end;
 
-            charPos[s.charAt(end)] = end;
+            ans = Math.max(ans, end-start+1);
+
             end++;
+
+
         }
 
-
         return ans;
+        
     }
 }
